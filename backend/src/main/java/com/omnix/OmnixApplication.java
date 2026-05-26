ac0436619420cbda30963a8f4b413c0bd0ad065d
jonadabedev-cpu <jonadabedev@gmail.com>
2026-05-26 13:39:42 -0300

    Adicionado Backend

---
+++ backend/src/main/java/com/omnix/OmnixApplication.java
@@ -0,0 +1,13 @@
+package com.omnix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/omnix/OmnixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
